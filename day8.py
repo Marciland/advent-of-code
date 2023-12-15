5c8f232c46ae261a6b2f473db396bca28f119495
Marcel Witoschek <marcel.marciland@gmail.com>
2023-12-15 02:12:52 +0100

    day8 bruteforcing part 2

--- day8.py
+++ day8.py
@@ -45,6 +45,55 @@ def navigate_network(instructions, nodes) -> int:
                     break
 
 
+def is_start_node(node: str) -> bool:
+    return node[-1] == 'A'
+
+
+def is_end_node(node: str) -> bool:
+    return node[-1] == 'Z'
+
+
+def find_location(location: str, nodes: list, instruction: str):
+    for node in nodes:
+        if node[0] == location:
+            return node[1] if instruction == 'L' else node[2]
+
+
+def navigate_network_two(instructions, nodes) -> int:
+    '''
+    start at all nodes with locations ending in A
+    navigate with the instructions through nodes simultaneously
+    stop when all nodes reached a location ending with Z
+    return steps taken
+    '''
+    starting_locations = []
+    for node in nodes:
+        if is_start_node(node[0]):
+            starting_locations.append(node[0])
+    temp_list = starting_locations.copy()
+    hits = 0
+    steps = 0
+    while True:
+        for instruction in instructions:
+            # return steps if all nodes hit the end at the same time (hits = amount of nodes)
+            if hits == len(starting_locations):
+                return steps
+            # do not increase hits, instead set to hits each instruction
+            current_hits = 0
+            for location in starting_locations:
+                temp_list.remove(location)
+                next_location = find_location(location, nodes, instruction)
+                if is_end_node(next_location):
+                    current_hits += 1
+                temp_list.append(next_location)
+            hits = current_hits
+            starting_locations = temp_list.copy()
+            # increase each instruction anyways
+            steps += 1
+
+
 list_of_instructions, list_of_nodes = read_input()
 # part one
 print(navigate_network(list_of_instructions, list_of_nodes))
+# part two
+print(navigate_network_two(list_of_instructions, list_of_nodes))
