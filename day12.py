a1c8c342371cc6a8db147115914aa52a59bed83f
Marcel Witoschek <marcel.marciland@gmail.com>
2023-12-19 04:26:37 +0100

    wip day12

--- day12.py
+++ day12.py
@@ -25,30 +25,98 @@ def read_input() -> tuple[list[str], list[list[int]]]:
 
 def remove_obvious_arrangements(splits: list[str], numbers: list[int]):
     '''obvious if:'''
+    len_splits = 0
+    len_numbers = get_sum(numbers)
+    for sp in splits:
+        len_splits += len(sp)
+    if len_splits == len_numbers:
+        splits.clear()
+        numbers.clear()
+        return
+    idle = False
+    while True:
+        if idle:
+            break
+        if len(splits[0]) == numbers[0] and '?' not in splits[0]:
+            del splits[0]
+            del numbers[0]
+            continue
+        idle = True
     temp = splits.copy()
     for sp in temp:
-        # there are no ? in a sequence of #
         if '?' in sp:
             continue
-        # the length fits exactly once
         if numbers.count(len(sp)) == 1:
+            # print('remove', sp, 'from', splits, numbers, 'at', splits.index(sp), numbers.index(len(sp)))
             splits.remove(sp)
             numbers.remove(len(sp))
+    if len(splits) == len(numbers) == 0:
+        return
     temp = splits.copy()
     for sp in temp:
-        # do not change if there is not enough splits
+        if '?' in sp:
+            continue
         if len(numbers) != len(splits):
             continue
-        # the sequence is found excatly once>
-        if numbers.count(len(sp)) == 1:
+        if splits.index(sp) == numbers.index(len(sp)):
+            # print('remove', sp, 'from', splits, numbers, 'at', splits.index(sp), numbers.index(len(sp)))
             splits.remove(sp)
             numbers.remove(len(sp))
+    if len(splits) == len(numbers) == 0:
+        return
+    temp = splits.copy()
+    for sp in temp:
+        if len(sp) < 5:
+            continue
+        if len(numbers) != len(splits):
+            continue
+        if len(sp) not in numbers:
+            continue
+        if splits.index(sp) != numbers.index(len(sp)):
+            continue
+        if numbers.count(len(sp)) == 1:
+            # print('remove', sp, 'from', splits, numbers, 'at', splits.index(sp), numbers.index(len(sp)))
+            splits.remove(sp)
+            numbers.remove(len(sp))
+    if len(splits) == len(numbers) == 0:
+        return
+    temp = splits.copy()
+    for sp in temp:
+        if sp.count('#') not in numbers:
+            continue
+        if numbers.count(sp.count('#')) != 1:
+            continue
+        if len(splits) != len(numbers):
+            continue
+        if sp.count('?') > numbers[numbers.index(sp.count('#'))]:
+            continue
+        if '#' * numbers[numbers.index(sp.count('#'))] not in sp:
+            continue
+        if splits.index(sp) == numbers.index(sp.count('#')):
+            # print('remove', sp, 'from', splits, numbers, 'at', splits.index(sp), numbers.index(sp.count('#')))
+            splits.remove(sp)
+            numbers.remove(sp.count('#'))
+    if len(splits) == len(numbers) == 0:
+        return
+    temp = splits.copy()
+    for sp in temp:
+        if '?' in sp:
+            continue
+        if len(numbers) != len(splits):
+            continue
+        # print('remove', sp, 'from', splits, numbers, 'at', splits.index(sp), numbers[splits.index(sp)])
+        del numbers[splits.index(sp)]
+        splits.remove(sp)
+    if len(splits) == len(numbers) == 0:
+        return
 
 
 def get_possible_arrangements(springs: str, numbers: list[int]) -> int:
     '''get possible arrangements of damaged springs'''
     splits = [x for x in springs.split('.') if x]
     remove_obvious_arrangements(splits, numbers)
+    if len(splits) == len(numbers) == 0:
+        return 1
     # find possible arrangements
     return 0
 
@@ -59,8 +127,8 @@ def solve_part_one():
     assert len(springs) == len(numbers)
     possible_arrangements = []
     for i in range(0, len(springs), 1):
-        possible_arrangements.append(
-            get_possible_arrangements(springs[i], numbers[i]))
+        possible_arrangements.append(get_possible_arrangements(springs[i],
+                                                               numbers[i]))
     print(get_sum(possible_arrangements))
 
 
